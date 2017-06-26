269db0825a0bf1adb2e9da41d786f1037e62d3f3
duncanaforbes <daf@cappui.com>
2017-06-26 16:14:55 +0100

    updated  automated_build.sh

--- automated_build.sh
+++ automated_build.sh
@@ -11,7 +11,7 @@ git clone https://github.com/thecheesemix/cheesemix-blog build
 cd $CWD
 bundle exec middleman contentful --rebuild
 
-# Push newly built repository
+au_# Push newly built repository
 cp -r $CWD/build/* /tmp/build
 
 cd /tmp/build
@@ -23,28 +23,3 @@ git add .
 git commit -m "Publishing Automated Cheesemix Blog Build Process"
 git push -f origin master
 
-
-# Copy static site
-CWD=`pwd`
-
-# Clone Pages repository
-cd /tmp
-git clone YOUR_PAGES_REPO build
-
-# cd build && git checkout -b YOUR_BRANCH origin/YOUR_BRANCH # If not using master
-
-# Trigger Middleman rebuild
-cd $CWD
-bundle exec middleman contentful --rebuild
-
-# Push newly built repository
-cp -r $CWD/build/* /tmp/build
-
-cd /tmp/build
-
-git config --global user.email "YOUR_EMAIL@example.com"
-git config --global user.name "YOUR NAME"
-
-git add .
-git commit -m "Automated Rebuild"
-git push -f origin YOUR_PAGES_BRANCH
