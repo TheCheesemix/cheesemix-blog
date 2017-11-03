75721b33cafb915f5f0d90a9d0f36a74ec8c4292
DuncanAForbes <daf@cappui.com>
2017-11-03 16:40:20 +0000

    updated automated_build.sh

--- automated_build.sh
+++ automated_build.sh
@@ -12,9 +12,9 @@ cd $CWD
 bundle exec middleman contentful --rebuild
 
 # Push newly built repository
-cp -r $CWD/build/* ./build
+cp -r $CWD/build/* /build
 
-cd ./build
+cd /build
 
 git config --global user.email "daf@cappui.com"
 git config --global user.name "duncanaforbes"
